60bd0432d7c39e75a5d32fee4cbf582dd200718b
Dmitry Shamrilo <shamrilods@gmail.com>
2021-02-23 22:18:31 +0300

    adds solar system

--- src/js/app.js
+++ src/js/app.js
@@ -1,33 +1,98 @@
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { GUI } from 'dat.gui'
+import { AxisGridHelper } from './axes-grid-helper'
 
-const FOV = 75;
-const NEAR = 0.1;
-const FAR = 1000;
+import sunTexture from '../images/sun-texture.jpeg'
+import earthTexture from '../images/earth-texture.jpeg'
+import moonTexture from '../images/moon-texture.jpg'
+
+const FOV = 40
+const NEAR = 0.1
+const FAR = 1000
 
 class App {
+  constructor() {
+    this.objects = []
+    this.gui = new GUI()
+    this.loader = new THREE.TextureLoader()
+  }
+
   init () {
     this.renderer = new THREE.WebGLRenderer({
       canvas: document.querySelector('#canvas')
     })
 
+    const aspect = window.innerWidth / window.innerHeight
+    this.camera = new THREE.PerspectiveCamera(FOV, aspect, NEAR, FAR)
+    this.camera.position.set(0, 50, 0)
+    this.camera.up.set(0, 0, 1)
+    this.camera.lookAt(0, 0, 0)
+
     this.scene = new THREE.Scene()
     this.scene.background = new THREE.Color(0x333333)
 
-    const aspect = window.innerWidth / window.innerHeight
-    this.camera = new THREE.PerspectiveCamera(FOV, aspect, NEAR, FAR)
-    this.camera.position.z = 2
+    const sphereGeometry = new THREE.SphereGeometry(1, 100, 100)
 
-    this.light = new THREE.DirectionalLight(0xFFFFFF, 1)
-    this.light.position.set(-1, 2, 4)
-    this.scene.add(this.light)
+    const solarSystem = new THREE.Object3D()
+    this.scene.add(solarSystem)
+    this.objects.push(solarSystem)
 
-    const cubeGeometry = new THREE.BoxGeometry(1, 1, 1)
-    const cubeMaterial = new THREE.MeshLambertMaterial({color: 0x44aa88})
-    this.cube = new THREE.Mesh(cubeGeometry, cubeMaterial)
-    this.scene.add(this.cube)
+    const sunMesh = new THREE.Mesh(
+      sphereGeometry,
+      new THREE.MeshBasicMaterial({
+        map: this.loader.load(sunTexture)
+      })
+    )
+    sunMesh.scale.set(5, 5, 5)
+    sunMesh.rotation.x = Math.PI / 2
+    solarSystem.add(sunMesh)
+    this.objects.push(sunMesh)
 
-    this.renderer.setPixelRatio(window.devicePixelRatio);
+    const earthOrbit = new THREE.Object3D()
+    earthOrbit.position.x = 10
+    solarSystem.add(earthOrbit)
+    this.objects.push(earthOrbit)
+
+    const earthMesh = new THREE.Mesh(
+      sphereGeometry,
+      new THREE.MeshBasicMaterial({
+        map: this.loader.load(earthTexture)
+      })
+    )
+    earthOrbit.add(earthMesh)
+    this.objects.push(earthMesh)
+
+    const moonOrbit = new THREE.Object3D()
+    moonOrbit.position.x = 2
+    earthOrbit.add(moonOrbit)
+
+    const moonMesh = new THREE.Mesh(
+      sphereGeometry,
+      new THREE.MeshBasicMaterial({
+        map: this.loader.load(moonTexture)
+      })
+    )
+    moonMesh.scale.set(.5, .5, .5)
+    moonOrbit.add(moonMesh)
+    this.objects.push(moonMesh)
+
+    const makeAxisGrid = (node, label, units) => {
+      const helper = new AxisGridHelper(node, units)
+      this.gui.add(helper, 'visible').name(label)
+    }
+
+    makeAxisGrid(solarSystem, 'solarSystem', 26)
+    makeAxisGrid(sunMesh, 'sunMesh')
+    makeAxisGrid(earthOrbit, 'earthOrbit')
+    makeAxisGrid(earthMesh, 'earthMesh')
+    makeAxisGrid(moonOrbit, 'moonOrbit')
+    makeAxisGrid(moonMesh, 'moonMesh')
+
+    this.renderer.setPixelRatio(window.devicePixelRatio)
     this.renderer.setSize(window.innerWidth, window.innerHeight)
+
+    new OrbitControls(this.camera, this.renderer.domElement)
     this.render()
   }
 
@@ -44,16 +109,17 @@ class App {
   }
 
   render () {
-    this.cube.rotation.x += 0.005
-    this.cube.rotation.y += 0.005
-
     if (this.resizeRendererToDisplaySize())  {
       const canvas = this.renderer.domElement
       this.camera.aspect = canvas.clientWidth / canvas.clientHeight
       this.camera.updateProjectionMatrix()
     }
 
-    this.renderer.render(this.scene, this.camera);
+    this.objects.forEach((obj) => {
+      obj.rotation.y += 0.005
+    })
+
+    this.renderer.render(this.scene, this.camera)
     requestAnimationFrame(() => this.render())
   }
 }
